daea5c226d147a721dd0eb22db2ffbf93ca26503
Benjamin Kray <benkray@gmail.com>
2017-10-15 20:37:52 -0700

    Added Swift initializer section

--- parser.py
+++ parser.py
@@ -1,21 +1,53 @@
 import json
+import re
+from enum import Enum
 
 def is_primitive(data_type):
     return isinstance(data_type, str) or isinstance(data_type, int) or isinstance(data_type, float)
 
+# Converts a string to either camelCase or PascalCase.
+def convert_case(value, toPascal=False):
+    if '_' in value:
+        components = value.split('_')
+    else:
+        components = re.findall('[A-Z][a-z]*', value)
+    if toPascal:
+        if len(components) > 0:
+            return ''.join(map(str.capitalize, components))
+        else:
+            return value.capitalize()
+    else:
+        if len(components) > 0:
+            return components[0] + ''.join(map(str.capitalize, components[1:]))
+        else:
+            return value
+
 class Model:
     def __init__(self, name='', properties=[]):
         self.properties = properties
-        self.name = name
-        for prop in properties:
-            print(prop.name + ': ' + str(prop.data_type))
+        self.name = convert_case(name, toPascal=True)
+
+class DataType:
+    def __init__(self, type_name=''):
+        self.is_primitive = True
+        if 'class \'str\'' in type_name:
+            self.type_name = 'String'
+        elif 'class \'int\'' in type_name:
+            self.type_name = 'Int'
+        elif 'class \'float\'' in type_name:
+            self.type_name = 'Float'            
+        else:
+            self.is_primitive = False
+            self.type_name = convert_case(type_name, toPascal=True)
 
 class Property:
     # Note: data_type can be a primitive (str, int, float), an array of primitives, or a custom type
     # Note: An array of primitives is denoted as <type>-list
-    def __init__(self, name='', data_type=None):
-        self.name = name
-        self.data_type = data_type
+    def __init__(self, name='', data_type=None, is_list=False):
+        self.json_field_name = name
+        self.name = convert_case(name, toPascal=False)
+        self.data_type = DataType(type_name=data_type)
+        self.is_list = is_list
 
 class Parser:
     def __init__(self, root_name=''):
@@ -53,13 +85,12 @@ class Parser:
             elif isinstance(value, list):
                 # Check for primitives, otherwise keep traversing
                 if is_primitive(value[0]):
-                    list_type = str(type(value[0])) + '-list'
-                    properties.append(Property(name=key, data_type=list_type))
+                    list_type = str(type(value[0]))
+                    properties.append(Property(name=key, data_type=list_type, is_list=True))
                 else:
-                    properties.append(Property(name=key, data_type=key))
+                    properties.append(Property(name=key, data_type=key, is_list=True))
                     self.parse_impl(value[0], key)
             else:
                 # We've reached a leaf node
-                properties.append(Property(name=key, data_type=type(value)))
-           
+                properties.append(Property(name=key, data_type=str(type(value))))
         self.models.append(Model(name=name, properties=properties))
--- swift_writer.py
+++ swift_writer.py
@@ -4,15 +4,36 @@ class SwiftWriter:
     def __init__(self, author='', company=''):
         self.author = author
         self.company = company
- 
+
     def generate(self, model):
         with open(model.name + '.swift', 'w+') as f:
             self.__generate_header(f)
             f.write('import Foundation\n\n')
             f.write('public class ' + model.name + ' {\n')
+
+            # Properties
             f.write('\n\t// MARK: - Properties\n\n')
             for prop in model.properties:
-                f.write('\tvar ' + prop.name + ': ' + str(prop.data_type) + '?\n')
+                data_type =  ('[' + prop.data_type.type_name + ']') if prop.is_list else prop.data_type.type_name
+                f.write('\tvar ' + prop.name + ': ' + data_type + '?\n')
+
+            # Initializers
+            f.write('\n\t// MARK: - Initializers\n\n')
+            f.write('\tpublic init(json: [String: Any]) {\n')
+            for prop in model.properties:
+                assignment = ''
+                if prop.is_list:
+                    if prop.data_type.is_primitive:
+                        assignment = 'if let items = json["' + prop.json_field_name + '"] as? [' + prop.data_type.type_name + '] { ' + prop.name + ' = items.map { ' + prop.data_type.type_name + '(json: $0) } }'
+                    else:
+                        assignment = 'if let items = json["' + prop.json_field_name + '"] as? [[String: Any]] { ' + prop.name + ' = items.map { ' + prop.data_type.type_name + '(json: $0) } }'
+                else:
+                    if prop.data_type.is_primitive:
+                        assignment = prop.name + ' = ' + 'json["' + prop.json_field_name + '"] as? ' + prop.data_type.type_name
+                    else:
+                        assignment = 'if let item = json["' + prop.json_field_name + '"] as? [String: Any] { ' + prop.name + ' = ' + prop.data_type.type_name + '(json: item) }'
+                f.write('\t\t' + assignment + '\n')
+            f.write('\t}\n')
             f.write('}\n')
     
     def __generate_header(self, model_file):
