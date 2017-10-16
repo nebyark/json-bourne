798394e7e82cd4d4e747174a64936f5bc1c71d11
Benjamin Kray <benkray@gmail.com>
2017-10-15 23:58:43 -0700

    Added method section to Swift Writer

--- swift_writer.py
+++ swift_writer.py
@@ -24,7 +24,7 @@ class SwiftWriter:
                 assignment = ''
                 if prop.is_list:
                     if prop.data_type.is_primitive:
-                        assignment = 'if let items = json["' + prop.json_field_name + '"] as? [' + prop.data_type.type_name + '] { ' + prop.name + ' = items.map { ' + prop.data_type.type_name + '(json: $0) } }'
+                        assignment = 'if let items = json["' + prop.json_field_name + '"] as? [' + prop.data_type.type_name + '] { ' + prop.name + ' = items.flatMap { $0 } }'
                     else:
                         assignment = 'if let items = json["' + prop.json_field_name + '"] as? [[String: Any]] { ' + prop.name + ' = items.map { ' + prop.data_type.type_name + '(json: $0) } }'
                 else:
@@ -33,6 +33,26 @@ class SwiftWriter:
                     else:
                         assignment = 'if let item = json["' + prop.json_field_name + '"] as? [String: Any] { ' + prop.name + ' = ' + prop.data_type.type_name + '(json: item) }'
                 f.write('\t\t' + assignment + '\n')
+            f.write('\t}\n')
+
+            # Methods
+            f.write('\n\t// MARK: - Methods\n\n')
+            f.write('\tpublic func toDictionary() -> [String: Any] {\n')
+            f.write('\t\tvar dictionary: [String: Any] = [:]\n')
+            for prop in model.properties:
+                assignment = ''
+                if prop.is_list:
+                    if prop.data_type.is_primitive:
+                        assignment = 'if let value = ' + prop.name + ' { dictionary["' + prop.json_field_name + '"] = value }\n'
+                    else:
+                        assignment = 'if let value = ' + prop.name + ' { dictionary["' + prop.json_field_name + '"] = value.map { $0.toDictionary() } }\n'
+                else:
+                    if prop.data_type.is_primitive:
+                        assignment = 'if let value = ' + prop.name + ' { dictionary["' + prop.json_field_name + '"] = value }\n'
+                    else:
+                        assignment = 'if let value = ' + prop.name + ' { dictionary["' + prop.json_field_name + '"] = value.toDictionary() }\n'
+                f.write('\t\t' + assignment) 
+            f.write('\t\treturn dictionary\n')
             f.write('\t}\n')
             f.write('}\n')
     
